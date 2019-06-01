9b231114eb2c039ebeb0ed0c83b47cfcfb83acbd
hendisantika <hendisantika@yahoo.co.id>
2019-06-01 09:36:27 +0700

    #11 - Add getDemoEs API in DemoResource Controller

--- src/main/java/com/hendisantika/springboottogglzdemo/controller/DemoResource.java
+++ src/main/java/com/hendisantika/springboottogglzdemo/controller/DemoResource.java
@@ -1,7 +1,10 @@
 package com.hendisantika.springboottogglzdemo.controller;
 
+import com.hendisantika.springboottogglzdemo.config.FeatureToggles;
 import com.hendisantika.springboottogglzdemo.service.SomeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +23,12 @@ public class DemoResource {
 
     @Autowired
     private SomeService someService;
+
+    @GetMapping(value = "/demo-es/{id}")
+    public String getDemoEs(@PathVariable("id") String id) {
+        if (FeatureToggles.TEXT_BASED_SEARCH_VIA_ELASTIC_SEARCH.isActive()) {
+            return "New ElasticSearch Backend is active";
+        }
+        return "Still relying in existing / legacy RDBMS backend implementation";
+    }
 }
