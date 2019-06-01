513b71844c8098d23a7f461c9ecf8258d3f683d6
hendisantika <hendisantika@yahoo.co.id>
2019-06-01 09:32:31 +0700

    #5 - Add SomeService

---
+++ src/main/java/com/hendisantika/springboottogglzdemo/service/SomeService.java
@@ -0,0 +1,15 @@
+package com.hendisantika.springboottogglzdemo.service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-togglz-demo
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-06-01
+ * Time: 09:32
+ */
+public interface SomeService {
+
+    String getSomeValue();
+}
