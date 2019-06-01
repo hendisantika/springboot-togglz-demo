b1f1c9a8e75df1f32956d86baf0c1fd177fe639e
hendisantika <hendisantika@yahoo.co.id>
2019-06-01 09:25:07 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springboottogglzdemo/SpringbootTogglzDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboottogglzdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTogglzDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTogglzDemoApplication.class, args);
+    }
+
+}
